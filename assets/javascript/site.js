b49161d855083baf8e0bcc594154e7be2f5dd820
bicks79 <nicholson.lcp@gmail.com>
2020-09-07 22:31:42 +0000

    add - site.js added.

---
+++ assets/javascript/site.js
@@ -0,0 +1,21 @@
+
+/*--------------------------------------------------Music*/
+
+/*Landing Page*/
+$(document).ready(function() {
+let theme = new Audio("/assets/audio/music/theme.mp3");
+theme.currentTime = 0;
+theme.loop = true;
+theme.play();
+});
+
+
+/*Menu Page*/
+
+
+/*--------------------------------------------------Music*/
+
+/*--------------------------------------------------Sound Effects*/
+
+
+
