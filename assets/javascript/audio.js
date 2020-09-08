a7612f7902154ac76f5f189a7c67b908436797f8
bicks79 <nicholson.lcp@gmail.com>
2020-09-08 12:28:47 +0000

    add - audio.js game.js - gameplay working.

--- assets/javascript/audio.js
+++ assets/javascript/audio.js
@@ -1,16 +1,7 @@
 
 /*--------------------------------------------------Music*/
 
-/*Landing Page*/
-$(document).ready(function() {
-let theme = new Audio("/assets/audio/music/theme.mp3");
-theme.currentTime = 0;
-theme.loop = true;
-theme.play();
-});
 
-
-/*Menu Page*/
 
 
 /*--------------------------------------------------Music*/
