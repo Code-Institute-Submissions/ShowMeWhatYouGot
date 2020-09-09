d83ca731d06a49b8e102ac07091ea71cfeed2868
bicks79 <nicholson.lcp@gmail.com>
2020-09-09 22:50:06 +0000

    add - second gameboard added morty.html along with morty.js. Modify - game.js is now rick.js

--- assets/javascript/morty.js
+++ assets/javascript/morty.js
@@ -1,20 +1,22 @@
-/*---------------------------------------------------3X3 Grid*/
+/*---------------------------------------------------4X4 Grid*/
 
 function swapTiles(cell1,cell2) {
   var temp = document.getElementById(cell1).className;
   document.getElementById(cell1).className = document.getElementById(cell2).className;
   document.getElementById(cell2).className = temp;
+  console.log(cell1)
+  console.log(cell2)
 }
 
 function shuffle() {
-//Use nested loops to access each cell of the 3x3 grid
-for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
-   for (var column=1;column<=3;column++) { //For each column in this row
+//Use nested loops to access each cell of the 4x4 grid
+for (var row=1;row<=4;row++) { //For each row of the 4x4 grid
+   for (var column=1;column<=4;column++) { //For each column in this row
   
-    var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
-    var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
+    var row3=Math.floor(Math.random()*4 + 1); //Pick a random row from 1 to 4
+    var column3=Math.floor(Math.random()*4 + 1); //Pick a random column from 1 to 4
      
-    swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
+    swapTiles("cell"+row+column,"cell"+row3+column3); //Swap the look & feel of both cells
   } 
 } 
 }
@@ -22,31 +24,31 @@ for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
 function clickTile(row,column) {
   var cell = document.getElementById("cell"+row+column);
   var tile = cell.className;
-  if (tile!="tile9") { 
+  if (tile!="tile16-4x4") { 
        //Checking if white tile on the right
-       if (column<3) {
-         if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
+       if (column<4) {
+         if ( document.getElementById("cell"+row+(column+1)).className=="tile16-4x4") {
            swapTiles("cell"+row+column,"cell"+row+(column+1));
            return;
          }
        }
        //Checking if white tile on the left
        if (column>1) {
-         if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
+         if ( document.getElementById("cell"+row+(column-1)).className=="tile16-4x4") {
            swapTiles("cell"+row+column,"cell"+row+(column-1));
            return;
          }
        }
          //Checking if white tile is above
        if (row>1) {
-         if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
+         if ( document.getElementById("cell"+(row-1)+column).className=="tile16-4x4") {
            swapTiles("cell"+row+column,"cell"+(row-1)+column);
            return;
          }
        }
        //Checking if white tile is below
-       if (row<3) {
-         if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
+       if (row<4) {
+         if ( document.getElementById("cell"+(row+1)+column).className=="tile16-4x4") {
            swapTiles("cell"+row+column,"cell"+(row+1)+column);
            return;
          }
